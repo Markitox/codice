80c9ed08a0fbbbff37d2765f41348f7036170bfa
Marco A. Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2009-07-02 23:22:41 -0500

    i18n

--- grails-app/taglib/CmsTagLib.groovy
+++ grails-app/taglib/CmsTagLib.groovy
@@ -9,7 +9,7 @@ class CmsTagLib {
 		if(cms) {
 			out << "<ul>"
 			cms.primaryLinks.menuItems.sort { m1,m2 -> m1.priority - m2.priority }.each{ item ->
-				out << "<li><a href='${item.url}'>${item.title}</a></li>"
+				out << "<li><a href='${item.url}'>${g.message( code:item.title )}</a></li>"
 			}
 			out << body()
 			out << "</ul>"
